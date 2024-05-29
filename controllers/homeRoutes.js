b6958e722539311beea690d013456b930b8c24ff
Austin-Marlatt <148661094+Austin-Marlatt@users.noreply.github.com>
2024-05-28 17:45:54 -0700

    Update homeRoutes.js
    Signed-off-by: Austin-Marlatt <148661094+Austin-Marlatt@users.noreply.github.com>

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,29 +4,17 @@ const { User } = require('../models');
 router.get (`/`, async (req, res) =>{
 try {
   let newProf = req.query.displayName;
-  const currentUser = await User.findOne({ where: { display_name: newProf} });
-  // const currentUser = userData.get({ plain: true });
-  res.render(`homepage`, {
-      currentUser,
-  });
+
+  const userData = await User.findOne({
+    where: { display_name: newProf}
+});
+    const currentUser = userData.get({ plain: true });
+
+  res.render(`homepage`, { currentUser });
 } catch (err) {
-    res.status(404).json(err);
+    console.log(err);
+    res.status(500).json(err);
 }
 });
-
-// router.get (`/profile`, async (req, res) =>{
-//   try {
-//     const userData = await User.findOne({ where: { display_name: req.query.display_name} });
-
-//     const currentUser = userData.map((user) => user.get({ plain: true }));
-
-//     res.render(`profile`, {
-//       ...currentUser,
-//   });
-//   } catch (err) {
-//   res.status(404).json(err);
-//   }
-// });
-
 
 module.exports = router;
